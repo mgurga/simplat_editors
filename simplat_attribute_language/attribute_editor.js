04a42c0c9152507323e5bd33ae46a7f6a699a360
rokie <motiurga@gmail.com>
2018-11-27 22:47:22 -0800

    now outputs error messages instead of just highlighting the problematic code

--- simplat_attribute_language/attribute_editor.js
+++ simplat_attribute_language/attribute_editor.js
@@ -7,12 +7,18 @@ var editor = CodeMirror(document.getElementById("codemirrorDiv"), {
     styleActiveLine: false
 });
 
+var errorLine = -1;
+
 function convert() {
 
     var readingAtts = false;
+    var selectedIds = [];
     var output = {};
     var input = editor.getValue();
     var rawInput = [];
+    var currentlySelecting = ""; // "textures" or "levels"
+    editor.setValue(input);
+    document.getElementById("errorDisplay").innerHTML = "";
     input = input.split("\n");
     rawInput = input;
     for (var i = 0; i < input.length; i++) {
@@ -20,41 +26,44 @@ function convert() {
     }
     console.log(input);
 
-    editor.markText({
-        line: 0,
-        ch: 0
-    }, {
-        line: input.length,
-        ch: rawInput.length
-    }, {
-        className: "transparent"
-    });
-
     for (var i = 0; i < input.length; i++) {
         if (input[i].includes("{")) {
             readingAtts = true;
-            console.log(input[i].includes(".") == true);
-            console.log(input[i].includes("/") == true);
-            if (!(input[i].includes(".") || input[i].includes("/"))) {
-                //console.log(input[i].includes(".") + "," + input[i].includes("/"));
-                editor.markText({
-                    line: i,
-                    ch: 0
-                }, {
-                    line: i,
-                    ch: rawInput[i].length
-                }, {
-                    className: "styled-background"
-                });
+
+            if (!(input[i].includes(".")) && !(input[i].includes("/"))) {
+                writeError("line " + i + " does not include a '.' or '/' but still lists IDs and has a starting curly bracket", i, rawInput[i].length);
             }
+
+            if (input[i].charAt(0) == ".") {
+                currentlySelecting = "textures";
+            } else {
+                currentlySelecting = "levels";
+            }
+
+            selectedIds = input[i].substring(0, input[i].length - 2);
+            console.log(selectedIds);
+
         }
         if (input[i].includes("}")) {
             readingAtts = false;
         }
         if (readingAtts && !(input[i].includes("{")) && !(input[i].includes("}"))) {
-            console.log(input[i]);
+            //console.log(input[i]);
         }
 
     }
 
+}
+
+function writeError(err, line, lineLength) {
+    document.getElementById("errorDisplay").innerHTML = err;
+    editor.markText({
+        line: line,
+        ch: 0
+    }, {
+        line: line,
+        ch: lineLength
+    }, {
+        className: "styled-background"
+    });
 }
